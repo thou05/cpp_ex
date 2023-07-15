6849647cdb606ebaa6a53720d4f1840e8b84907d
Thao Le <139248123+thou05@users.noreply.github.com>
2023-07-16 05:58:00 +0700

    vonglaphcn.cpp

--- vonglaphcn.cpp
+++ vonglaphcn.cpp
@@ -13,19 +13,3 @@ int main(){
 	}
 
 }
-
-int min(){
-	int n, m;
-	cin >> n >> m;
-	for (int i=1; i<=n; i++){
-		for (int j=1; j<=m; j++){
-			if ( i==1 || i==n || j==1 || j==m ){
-				cout << "*";
-			}
-			else{
-				cout << " ";
-			}
-		}
-		cout << endl;
-	}
-}
